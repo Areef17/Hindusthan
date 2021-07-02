27bd1794842e5adafad09d1cfa9ee1e58fe46387
areefshaik9030@gmail.com <shaikareef9030@gmail.com>
2021-07-02 21:55:11 +0530

    adding a new code

---
+++ src/HP1/HP.java
@@ -0,0 +1,12 @@
+package HP1;
+
+public class HP {
+
+	public static void main(String[] args) {
+		System.out.println("create a java projrect ");
+		System.out.println("Areeef is the new begineer");
+		System.out.println("How will solve the problem");
+
+	}
+
+}
